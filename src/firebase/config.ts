f7042917ebc9f495d0a7ec50f5854be0ad237058
ALPHA MOTORS <contact@alphamotors-cameroun.com>
2025-12-22 10:14:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7341482816-c42ac",
+  "appId": "1:102008737178:web:9601471a8cee3f235b7e23",
+  "apiKey": "AIzaSyD1NqXfUa8Ji4sElJZqKuNjJpYujQ890z4",
+  "authDomain": "studio-7341482816-c42ac.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "102008737178"
+};
